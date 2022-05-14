87febe943bb0ce606642d2dc279b60b10a7b1344
johndoeteddy <johndoeteddy1@gmail.com>
2022-05-14 10:42:36 +0200

    implemented FilterComponents

--- Source/UI/FilterComponent.cpp
+++ Source/UI/FilterComponent.cpp
@@ -10,3 +10,59 @@
 
 #include <JuceHeader.h>
 #include "FilterComponent.h"
+
+//==============================================================================
+FilterComponent::FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce::String filterID) : filterID(filterID)
+{
+    // In your constructor, you should add any child components, and
+    // initialise any special settings that your component needs.
+    using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    
+    lowCutFreqAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("LowCutFreq"), lowCutFreqSlider);
+    highCutAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("Decay"), highCutFreqSlider);
+    peakFreqAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("Sustain"), peakFreqSlider);
+    peakGainInDecibelsAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("Release"), peakGainInDecibelsSlider);
+    peakQualityAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("Decay"), peakQualitySlider);
+
+    setParameterStyle(lowCutFreqSlider);
+    setParameterStyle(highCutFreqSlider);
+    setParameterStyle(peakFreqSlider);
+    setParameterStyle(peakGainInDecibelsSlider);
+    setParameterStyle(peakQualitySlider);
+}
+
+FilterComponent::~FilterComponent()
+{
+}
+
+void FilterComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (juce::Colours::black);
+}
+
+void FilterComponent::resized()
+{
+    const auto bounds = getLocalBounds().reduced(10);
+    const auto padding = 10;
+    const auto sliderWidth = bounds.getWidth() / 4 - padding;
+    const auto sliderHeight = bounds.getHeight();
+    const auto sliderStartX = 0;
+    const auto sliderStartY = 0;
+    
+    lowCutFreqSlider.setBounds(sliderStartX, sliderStartY, sliderWidth, sliderHeight);
+    highCutFreqSlider.setBounds(lowCutFreqSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    peakFreqSlider.setBounds(highCutFreqSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    peakGainInDecibelsSlider.setBounds(peakFreqSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    peakQualitySlider.setBounds(peakGainInDecibelsSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+}
+
+void FilterComponent::setParameterStyle(juce::Slider& slider){
+    slider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 25);
+    addAndMakeVisible(slider);
+}
+
+// @TODO find a cleaner way of retrieving attachment keywords from apvts
+juce::String FilterComponent::getStringParameter(juce::String pName){
+    return filterID + pName;
+}
--- Source/UI/FilterComponent.h
+++ Source/UI/FilterComponent.h
@@ -11,3 +11,39 @@
 
 #include <JuceHeader.h>
 
+//==============================================================================
+/*
+*/
+class FilterComponent  : public juce::Component
+{
+public:
+    FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce::String filterID);
+    ~FilterComponent() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // used to differentiate between potentially multiple filters
+    juce::String filterID;
+    
+    juce::Slider lowCutFreqSlider;
+    juce::Slider highCutFreqSlider;
+    juce::Slider peakFreqSlider;
+    juce::Slider peakGainInDecibelsSlider;
+    juce::Slider peakQualitySlider;
+    
+    //alias
+    using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    
+    std::unique_ptr<SliderAttachment> lowCutFreqAttachment;
+    std::unique_ptr<SliderAttachment> highCutAttachment;
+    std::unique_ptr<SliderAttachment> peakFreqAttachment;
+    std::unique_ptr<SliderAttachment> peakGainInDecibelsAttachment;
+    std::unique_ptr<SliderAttachment> peakQualityAttachment;
+    
+    void setParameterStyle(juce::Slider& slider);
+    juce::String getStringParameter(juce::String pName);
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FilterComponent)
+};
