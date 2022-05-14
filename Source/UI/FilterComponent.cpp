62af1cc3e6ee7c4e0161d8f738ffb2f294bcab5d
johndoeteddy <johndoeteddy1@gmail.com>
2022-05-14 10:23:40 +0200

    added filterComponent files

---
+++ Source/UI/FilterComponent.cpp
@@ -0,0 +1,12 @@
+/*
+  ==============================================================================
+
+    FilterComponent.cpp
+    Created: 14 May 2022 10:58:00am
+    Author:  Jonathan Reymond
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+#include "FilterComponent.h"
