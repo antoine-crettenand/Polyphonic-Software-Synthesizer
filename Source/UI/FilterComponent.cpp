214a5c4de51daaeef2d824c0becc02c2091a9197
unknown <l456@houmard.com>
2022-06-13 19:57:19 +0200

    Update UI

--- Source/UI/FilterComponent.cpp
+++ Source/UI/FilterComponent.cpp
@@ -24,11 +24,30 @@ FilterComponent::FilterComponent(juce::AudioProcessorValueTreeState& apvts, juce
     peakGainInDecibelsAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("PeakGain"), peakGainInDecibelsSlider);
     peakQualityAttachment = std::make_unique<SliderAttachment>(apvts, getStringParameter("PeakQuality"), peakQualitySlider);
 
+
     setParameterStyle(lowCutFreqSlider);
     setParameterStyle(highCutFreqSlider);
     setParameterStyle(peakFreqSlider);
     setParameterStyle(peakGainInDecibelsSlider);
     setParameterStyle(peakQualitySlider);
+
+
+    lowCutFreqText.setText("Low cut", juce::dontSendNotification);
+    highCutFreqText.setText("High cut", juce::dontSendNotification);
+    peakFreqText.setText("Peak freq", juce::dontSendNotification);
+    peakGainText.setText("Peak gain", juce::dontSendNotification);
+    peakQualityText.setText("Peak quality", juce::dontSendNotification);
+
+    filterTitle.setButtonText("Filters");
+    filterTitle.setColour(juce::TextButton::textColourOffId, juce::Colours::lightblue);
+
+    addAndMakeVisible(lowCutFreqText);
+    addAndMakeVisible(highCutFreqText);
+    addAndMakeVisible(peakFreqText);
+    addAndMakeVisible(peakGainText);
+    addAndMakeVisible(peakQualityText);
+    addAndMakeVisible(filterTitle);
+
 }
 
 FilterComponent::~FilterComponent()
@@ -42,18 +61,27 @@ void FilterComponent::paint (juce::Graphics& g)
 
 void FilterComponent::resized()
 {
-    const auto bounds = getLocalBounds().reduced(20);
-    const auto padding = 5;
-    const auto sliderWidth = bounds.getWidth() / 4 - padding;
-    const auto sliderHeight = bounds.getHeight();
-    const auto sliderStartX = 0;
-    const auto sliderStartY = 0;
     
-    lowCutFreqSlider.setBounds(sliderStartX, sliderStartY, sliderWidth, sliderHeight);
-    highCutFreqSlider.setBounds(lowCutFreqSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-    peakFreqSlider.setBounds(highCutFreqSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-    peakGainInDecibelsSlider.setBounds(peakFreqSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-    peakQualitySlider.setBounds(peakGainInDecibelsSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    const auto sliderWidth = getWidth() / 6;
+    const auto sliderHeight = 3 * getHeight() / 5;
+    const int padding = getWidth()/36;
+    const int paddingTextVertical = getHeight() / 25;
+    const int textHeight = 3*getHeight() / 25;
+    
+    filterTitle.setBounds(2*getWidth() / 5, getHeight() / 10, getWidth() / 5, getHeight() / 10);
+
+    lowCutFreqSlider.setBounds(padding, filterTitle.getBottom() + padding, sliderWidth, sliderHeight);
+    lowCutFreqText.setBounds(padding, lowCutFreqSlider.getBottom() + paddingTextVertical, sliderWidth, textHeight);
+    highCutFreqSlider.setBounds(lowCutFreqSlider.getRight() + padding, filterTitle.getBottom() + padding, sliderWidth, sliderHeight);
+    highCutFreqText.setBounds(lowCutFreqSlider.getRight() + padding, highCutFreqSlider.getBottom() + paddingTextVertical, sliderWidth, textHeight);
+    peakFreqSlider.setBounds(highCutFreqSlider.getRight() + padding, filterTitle.getBottom() + padding, sliderWidth, sliderHeight);
+    peakFreqText.setBounds(highCutFreqSlider.getRight() + padding, peakFreqSlider.getBottom() + paddingTextVertical, sliderWidth, textHeight);
+    peakGainInDecibelsSlider.setBounds(peakFreqSlider.getRight() + padding, filterTitle.getBottom() + padding, sliderWidth, sliderHeight);
+    peakGainText.setBounds(peakFreqSlider.getRight() + padding, peakGainInDecibelsSlider.getBottom() + paddingTextVertical, sliderWidth, textHeight);
+    peakQualitySlider.setBounds(peakGainInDecibelsSlider.getRight() + padding, filterTitle.getBottom() + padding, sliderWidth, sliderHeight);
+    peakQualityText.setBounds(peakGainInDecibelsSlider.getRight() + padding, peakQualitySlider.getBottom() + paddingTextVertical, sliderWidth, textHeight);
+
+
 }
 
 void FilterComponent::setParameterStyle(juce::Slider& slider){
